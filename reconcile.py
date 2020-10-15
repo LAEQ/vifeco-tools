f0ab9a4e0572b092477c91182ed00af6a2845009
David Maignan <davidmaignan@gmail.com>
2020-10-15 09:54:09 -0400

    Refactor reconcile

--- reconcile.py
+++ reconcile.py
@@ -3,6 +3,8 @@ import json
 import glob
 import argparse
 
+from utils.reconcile import parseFile
+
 # Globals
 BASE_PATH = os.path.dirname(os.path.realpath(__file__))
 SOURCE_PATH = os.path.join(BASE_PATH, "source")
@@ -17,26 +19,10 @@ parser.add_argument("--target", "-t", default=TARGET_PATH,
                     help="Folder where the converted json will be saved on success")
 args = vars(parser.parse_args())
 
-
 # Paths from arguments
 MODEL_FILE = os.path.join(BASE_PATH, args['model'])
 SOURCE_PATH = args['source']
 TARGET_PATH = args['target']
-
-
-def reconcile(_category, models):
-    """
-    Return id from model for a category if names are equals
-    :param _category: cat to reconcile
-    :param models: List of categories from the model
-    :return: int|None
-    """
-    try:
-        return models[_category['name']]
-    except KeyError:
-        print("'{}' not found.".format(_category['name']))
-        print("Model list: {}".format([item for item in models.keys()]))
-        return None
 
 
 if __name__ == "__main__":
@@ -55,52 +41,12 @@ if __name__ == "__main__":
         print("Target path is not valid: {}".format(TARGET_PATH))
         exit(1)
 
-    with open(MODEL_FILE, 'r', encoding='utf8') as f:
-        model = json.load(f)
+    for src_file in glob.glob(SOURCE_FILES):
+        source_filename = os.path.basename(src_file)
+        print("Converting: {}".format(source_filename))
+        src_json = parseFile(src_file, MODEL_FILE)
 
-    modelSet = {item['name']: item['id'] for item in model['collection']['categorySet']}
-
-    # List of files
-    files = glob.glob(SOURCE_FILES)
-
-    for src_file in files:
-        src_name = os.path.basename(src_file)
-
-        with open(src_file, 'r', encoding='utf8') as reader:
-            print("Converting: {}".format(src_name))
-            src_json = json.load(reader)
-
-            categorySet = src_json['collection']['categorySet']
-            pointSet = src_json['pointSet']
-
-            # Validation: model and target must have same categories (number and names)
-            if len(modelSet) != len(categorySet):
-                print("{}'s collection has %d categories but the model has %d.".format(src_name, len(categorySet),
-                                                                                       len(modelSet)))
-            reconcile_ids = [None] * (max(categorySet, key=lambda x: x['id'])['id'] + 1)
-            for category in categorySet:
-                identifier = reconcile(category, modelSet)
-                # Error category names are not strictly equals: spaces, (upper|lower)case, encoding, accents, ...
-                if identifier is None:
-                    exit(1)
-
-                reconcile_ids[category['id']] = identifier
-                category['id'] = identifier
-
-            for point in pointSet:
-                try:
-                    id = reconcile_ids[point['categoryId']]
-                    point['categoryId'] = id
-                except:
-                    print("point: %s (file: %s) does not match a category of the model %s".format(
-                        point, src_name, args['model']
-                    ))
-                    exit(1)
-
-                # Update collection
-                src_json['collection'] = model['collection']
-
-            with open(os.path.join(TARGET_PATH, src_name), 'w', encoding='utf8') as writer:
-                json.dump(src_json, writer, ensure_ascii=False)
+        with open(os.path.join(TARGET_PATH, source_filename), 'w', encoding='utf8') as writer:
+            json.dump(src_json, writer, ensure_ascii=False)
 
     print(" ----> Reconciliation completed with success.")
---
+++ utils/reconcile.py
@@ -0,0 +1,62 @@
+import json
+
+
+def reconcile(_category, models):
+    """
+    Return id from model for a category if names are equals
+    :param _category: cat to reconcile
+    :param models: List of categories from the model
+    :return: int|None
+    """
+    try:
+        return models[_category['name']]
+    except KeyError:
+        print("'{}' not found.".format(_category['name']))
+        print("Model list: {}".format([item for item in models.keys()]))
+        return None
+
+
+def getCategorySet(file):
+    with open(file, 'r', encoding='utf8') as f:
+        video = json.load(f)
+
+    return {item['name']: item['id'] for item in video['collection']['categorySet']}
+
+
+def parseFile(source_file, model_file):
+    with open(model_file, 'r', encoding='utf8') as f:
+        model = json.load(f)
+
+    model_set = {item['name']: item['id'] for item in model['collection']['categorySet']}
+
+    with open(source_file, 'r', encoding='utf8') as r:
+        source_json = json.load(r)
+
+        category_set = source_json['collection']['categorySet']
+        point_set = source_json['pointSet']
+
+        # Validation: model and target must have same categories (number and names)
+        if len(model_set) != len(category_set):
+            print("Collection has %d categories but the model has %d.".format(len(category_set), len(model_set)))
+
+        reconcile_ids = [None] * (max(category_set, key=lambda x: x['id'])['id'] + 1)
+        for category in category_set:
+            identifier = reconcile(category, model_set)
+            # Error category names are not strictly equals: spaces, (upper|lower)case, encoding, accents, ...
+            if identifier is None:
+                exit(1)
+
+            reconcile_ids[category['id']] = identifier
+            category['id'] = identifier
+
+        for point in point_set:
+            try:
+                id = reconcile_ids[point['categoryId']]
+                point['categoryId'] = id
+            except:
+                print("point: %s does not match a category of the model ".format(point))
+                exit(1)
+
+        source_json['collection'] = model['collection']
+
+        return source_json
