37bfc1fa870d41d394e5eedbb88b4003499235e7
David Maignan <davidmaignan@gmail.com>
2020-10-15 10:52:35 -0400

    Set graph for tarjan algorithm

---
+++ tests/test_graph.py
@@ -0,0 +1,53 @@
+from unittest import TestCase
+
+from utils.model import Point, Graph
+
+
+class TestGraph(TestCase):
+    @staticmethod
+    def get_pts_1():
+        return [Point({"id": 17,
+                       "x": 0.3701188455008489,
+                       "y": 0.46934540652707035,
+                       "categoryId": 11,
+                       "startDouble": 1000 * i,
+                       "videoId": "1"
+                       }) for i in range(0, 3)]
+
+    @staticmethod
+    def get_pts_2():
+        return [Point({"id": 17,
+                       "x": 0.3701188455008489,
+                       "y": 0.46934540652707035,
+                       "categoryId": 11,
+                       "startDouble": 1000 * i,
+                       "videoId": "2"
+                       }) for i in range(0, 3)]
+
+    @staticmethod
+    def get_pts_2():
+        return [Point({"id": 17,
+                       "x": 0.3701188455008489,
+                       "y": 0.46934540652707035,
+                       "categoryId": 11,
+                       "startDouble": 1000 * i,
+                       "videoId": "2"
+                       }) for i in range(0, 3)]
+
+    def test_graph_init(self):
+        graph = Graph(self.get_pts_1(), self.get_pts_2())
+
+        self.assertEqual(len(graph.graph), 6)
+
+    def test_tarjan(self):
+        graph = Graph(self.get_pts_1(), self.get_pts_2())
+        tj = graph.tj
+
+        self.assertEqual(len(tj), 1)
+        self.assertEqual(len(tj[0]), 6)
+
+    def test_tarjan(self):
+        graph = Graph(self.get_pts_1(), self.get_pts_2(), delta=100)
+        result = [len(l) for l in graph.tj]
+
+        self.assertListEqual([2, 2, 2], result)
--- utils/model.py
+++ utils/model.py
@@ -1,4 +1,6 @@
-from uuid import uuid1
+from uuid import uuid4
+from typing import List
+from tarjan import tarjan
 
 
 class Point:
@@ -6,10 +8,38 @@ class Point:
         self.catgory_id = args['categoryId']
         self.start = args['startDouble']
         self.videoId = args['videoId']
-        self.uuid = uuid1()
+        self.uuid = uuid4()
 
     def match(self, other, delta=5000):
         if isinstance(other, Point):
             return self.catgory_id == other.catgory_id and abs(self.start - other.start) <= delta
 
         return False
+
+    def __repr__(self):
+        return "{%s: %s}" % (self.uuid, self.start)
+
+
+class Graph:
+    """
+    Graph of points
+    """
+    def __init__(self, pts_1: List[Point], pts_2: List[Point], delta=5000):
+        self.pts_1 = pts_1
+        self.pts_2 = pts_2
+        self.delta = delta
+
+        self.graph = {p.uuid: [] for p in self.pts_1}
+        self.graph.update({p.uuid: [] for p in self.pts_2})
+
+        for pt1 in self.pts_1:
+            for pt2 in self.pts_2:
+                if pt1.match(pt2, self.delta):
+                    self.graph[pt1.uuid].append(pt2.uuid)
+
+        for pt2 in self.pts_2:
+            for pt1 in self.pts_1:
+                if pt2.match(pt1, self.delta):
+                    self.graph[pt2.uuid].append(pt1.uuid)
+
+        self.tj = tarjan(self.graph)
